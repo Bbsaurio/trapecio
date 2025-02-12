6397e94eeb2897dbcc758ba027a285e9c3e8ae77
sosag <sosag@GAEL>
2025-02-12 16:32:38 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trapecio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Insertar base mayor (B)");
+            int B = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Insertar base menor (b)");
+            int b = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("Insertar altra (h)");
+            int h = int.Parse(Console.ReadLine());
+            int S = B + b;
+            int M = S * h;
+            int R = M / 2;
+            System.Console.WriteLine("El resultado es:" + R);
+            Console.ReadKey();
+        }
+    }
+}
